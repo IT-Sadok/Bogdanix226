0bcc891375a8d92ec966a067d20fc651d46b728e
Bohdan <peliak.bohdan@gmail.com>
2026-03-05 13:23:17 +0200

    add category for transactions

--- Finly/Finly/Data/AppDbContext.cs
+++ Finly/Finly/Data/AppDbContext.cs
@@ -15,4 +15,23 @@ public class AppDbContext : IdentityDbContext<User, IdentityRole<int>, int>
 
     public DbSet<Account> Accounts => Set<Account>();
     public DbSet<Transaction> Transactions => Set<Transaction>();
+    public DbSet<Category> Categories { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Food" },
+            new Category { Id = 2, Name = "Transport" },
+            new Category { Id = 3, Name = "Iban" },
+            new Category { Id = 4, Name = "Restaurant" },
+            new Category { Id = 5, Name = "Travells" },
+            new Category { Id = 6, Name = "Cinema" },
+            new Category { Id = 7, Name = "Health" },
+            new Category { Id = 8, Name = "Entertainment" },
+            new Category { Id = 9, Name = "Electronics" },
+            new Category { Id = 10, Name = "Utilities" }
+        );
+    }
 }
---
+++ Finly/Finly/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Finly.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+
+    public List<Transaction> Transactions { get; set; } = new();
+}
